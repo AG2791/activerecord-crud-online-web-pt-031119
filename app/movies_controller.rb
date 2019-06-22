74d68b664db978231fa1480fa3802dc026257834
AJ Guiles <ashiguiles@gmail.com>
2019-06-22 18:19:47 +0000

    Automatically backed up by Learn

--- app/movies_controller.rb
+++ app/movies_controller.rb
@@ -24,13 +24,13 @@ def can_be_created_with_a_hash_of_attributes
   move.save 
 end
 
-def can_be_created_in_a_block(args = __)
+def can_be_created_in_a_block(args = title ="Inception" release_date = 2008)
   # If no arguments are passed, use default values:
   # title == "Home Alone"
   # release_date == 1990
   
   Movie.create do |m|
-    __
+    
   end
 end
 
